4707768826de921f4e10ebcc7b52190601799615
jhone-rdz <jhone.rodrigues51@gmail.com>
2025-09-29 22:03:53 -0300

    att url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { authService } from './auth';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://petshop-backend-f4nc.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://petshop-backend-f4nc.onrender.com';
 
 export const authService = {
   login: (credentials) => 
